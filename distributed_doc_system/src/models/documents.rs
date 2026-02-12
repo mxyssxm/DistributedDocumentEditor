44f8feaa510e53951d8f106312f0f92d02ec8932
Salim Kaimoussi <salimkaimoussi0@gmail.com>
2026-02-12 22:36:29 +0100

    Mise à jour : Intégration de Zenoh

--- distributed_doc_system/src/models/documents.rs
+++ distributed_doc_system/src/models/documents.rs
@@ -1,47 +1,48 @@
-
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Document {
-    pub id : String,
-    pub name : String,
-    pub content : String,
-    pub version : usize,
-
+    pub doc_id: String, 
+    pub name: String,
+    pub content: String,
+    pub version: usize,
 }
 
-impl Document {
+impl Document {    // Constructeur pour Crée un nouveau document vide.
+    // Appelé quand le serveur reçoit le message ClientMessage::Create.
     pub fn new(name: String) -> Self {
-        Self{  
-            id: uuid::Uuid::new_v4().to_string(),
+        Self {
+            doc_id: uuid::Uuid::new_v4().to_string(), 
             name,
-            content : String::new(),
-            version : 0,  // Gestion de version pour les conflits
-
-
+            content: String::new(),    // Commence vide.
+            version: 0,                // Commence à la version 0.
         }
     }
 
     pub fn update(&mut self, new_content: String, base_version: usize) -> Result<usize, String> {
-        if base_version != Self.version{
-            return Err("Conflit de version : Le document a été modifié par un autre utilisateur.".to_String());
-
+        // VÉRIFICATION DE CONFLIT 
+        // On compare la version que le client PENSE avoir (base_version)
+        // avec la version RÉELLE du serveur (self.version).
+        
+        if base_version != self.version {
+            return Err("Conflit : Version obsolète.".to_string()); // On refuse la mise à jour pour ne pas écraser le travail de l'autre.
         }
-        self.content = new_content;
-        self.version += 1;
-        OK(self.version)
+        // SUCCÈS : Les versions correspondent.
+        self.content = new_content;  // 1. On applique le nouveau texte.
+        self.version += 1;  // 2. On incrémente la version locale
+        Ok(self.version) // On retourne la nouvelle version pour confirmer le succès
     }
 }
 
-// le stockage  en mémoire
 pub struct Repository {
+    // La clé de la Map est le doc_id
+    // Valeur (Document)
     pub docs: HashMap<String, Document>,
-    
 }
 
-impl Repository {
-    pub fn new() -> Self{
-        Self { docs: Hashmap:: new()}
-
+impl Repository {   // Initialise le stockage au démarrage du serveur (dans main.rs)
+    pub fn new() -> Self {
+        Self { docs: HashMap::new() }  // Démarre avec une liste vide
     }
 }
--- distributed_doc_system/src/models/message.rs
+++ distributed_doc_system/src/models/message.rs
@@ -1,38 +1,62 @@
-
 use actix::prelude::*;
 use serde::{Deserialize, Serialize};
 
-// les Messages envoyés par le Client -> Serveur
-pub enum ClientMessage {
-    Create { name : String},
-    Update { id : String, content : String, version : usize },
-    Join { id : String},
 
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(tag = "type", content = "payload")]  //onfigure le format JSON pour qu'il soit facile à lire par le frontend
+
+pub enum ClientMessage {   // Définit strictement les actions possibles de l'utilisateur
+    Create { name: String },
+    Update { doc_id: String, content: String, version: usize }, 
+    Join { doc_id: String }, 
 }
 
-// les Messages envoyés par le Serveur -> Client 
-pub enum ServerMessage {
-    DocCreated { id : String, name : String},
-    DocUpdate { id : String, content : String, version : String},
-    Error { message : String}, 
-    List { docs: Vec<(String, String)> },
+#[derive(Serialize, Deserialize, Debug, Clone)] // permet la conversion en JSON pour la réponse réseau.
+#[serde(tag = "type", content = "payload")]
 
+pub enum ServerMessage {   // SERVEUR -> CLIENT
+    DocCreated { doc_id: String, name: String }, 
+    DocUpdated { doc_id: String, content: String, version: usize }, 
+    Error { message: String },   // si on a un Conflit de version détecté
+    List { docs: Vec<(String, String)> }, // Envoie la liste des documents disponibles à la connexion.
 }
+
+
+
+#[derive(Message)]
+#[rtype(result = "()")] // Définit que l'acteur qui reçoit ce message ne renvoie RIEN
 
 pub struct WsMessage(pub String);
 
-// Connexion de  l'utilisateur
-pub struct Connect {
-    pub addr: Recipient<WsMessage>,
-    pub id: usize,
+#[derive(Message)]
+#[rtype(result = "()")]    //  Réponse vide attendue
+
+pub struct Connect {        // Événement : Un User ouvre une connexion WebSocket.
+    pub addr: Recipient<WsMessage>,     // L'adresse de réponse
+    pub client_id: usize, 
 }
 
-// Déconnexion
-pub struct Disconnect {
-    pub id: usize,
+#[derive(Message)]
+#[rtype(result = "()")]
+
+pub struct Disconnect {   // Événement : Un utilisateur ferme son onglet.
+    pub client_id: usize,   // Identifie quel utilisateur s'est déconnecté
 }
 
-pub struct ClientCommand{
-    pub id : usize,
-    pub cmd : ClientMessage,
+#[derive(Message)]
+#[rtype(result = "()")]
+
+pub struct ClientCommand {    
+    pub client_id: usize, 
+    pub cmd: ClientMessage,    // Contient le Create/Update/Join
+}
+
+
+#[derive(Message, Debug)]
+#[rtype(result = "()")]
+
+pub struct ZenohUpdate {       // L'Interface avec le Middleware "Pub/Sub"
+    pub key: String,          // (Key Expression) Permet de savoir QUEL document a été modifié.
+    pub payload: String,     // C'est le contenu réel  transporté par le middleware
 }
